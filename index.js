b067c25bfc1a0d626539f138b65556a76c67aeec
jsfgefge <68561882+Jsfgefge@users.noreply.github.com>
2024-02-19 11:03:59 -0600

    changes to index.html and index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+//#region Game Logic
+
+var fixedChoice;
+var playerCounter=0;
+var pcCounter=0;
+
+
 function getRandomInt(){
     return Math.ceil(Math.random()*3);
 }
 
-var pcChoice = function(){
+const pcChoice = function(){
     let choice = getRandomInt();
     if(choice == 1){
         return "Rock";
@@ -17,32 +24,57 @@ var pcChoice = function(){
 
 function playRound(playerSelection, pcSelection){
     var x1 = playerSelection.toLowerCase();
+    addPlayerPcChoice(playerSelection,pcSelection);
+
     if(pcSelection=="Scissors" && x1=="paper"){
-        return "You Lose! "+pcSelection+" beats "+playerSelection;
+        addOutcome("You Lose! "+pcSelection+" beats "+playerSelection);
+        return 0;
     }
     else if(pcSelection=="Rock" && x1=="scissors"){
-        return "You Lose! "+pcSelection+" beats "+playerSelection;
+        addOutcome("You Lose! "+pcSelection+" beats "+playerSelection);
+        return 0;
     }
     else if(pcSelection=="Paper" && x1=="rock"){
-        return "You Lose! "+pcSelection+" beats "+playerSelection;
+        addOutcome("You Lose! "+pcSelection+" beats "+playerSelection);
+        return 0;
     }
     else if(pcSelection.toLowerCase()==x1){
-        return "It's a draw! "+pcSelection+" it's the same as "+playerSelection;
+        addOutcome("It's a draw! "+pcSelection+" it's the same as "+playerSelection);
+        return 0;
     }
     else{
-        return "You Win! "+playerSelection+" beats "+pcSelection;
+        addOutcome("You Win! "+playerSelection+" beats "+pcSelection);
+        return 1;
     }
 
 }
 
 function playGameUI(playerSelection){
-    var isWin = playRound(playerSelection, pcChoice());
-    console.log(isWin);
+    fixedChoice = pcChoice();
+    var isWin = playRound(playerSelection, fixedChoice );
+    if(isWin == 1){
+        playerCounter+=1;
+    }else{
+        pcCounter+=1;
+    }
+    addResult();
 }
+
+//#endregion
+
+//#region UIControl
 
 const btnPaper = document.querySelector('#btnPaper');
 const btnRock = document.querySelector("#btnRock");
 const btnScissors = document.querySelector("#btnScissors");
+
+const resultsContainer = document.querySelector('.container.results');
+const choicesContainer = document.querySelector('.container.choices');
+
+const outcome = document.createElement('h3');
+const result = document.createElement('h3');
+const playerH5 = document.createElement("h5");
+const pcH5 = document.createElement("h5");
 
 btnPaper.addEventListener('click', () => {
     playGameUI('Paper');
@@ -54,10 +86,40 @@ btnScissors.addEventListener('click',()=>{
     playGameUI('Scissors');
 })
 
+function addPlayerPcChoice(player, pc){
+    playerH5.classList.add('selection');
+    pcH5.classList.add('selection');
+
+    playerH5.textContent = "player choice: "+player;
+    pcH5.textContent = "pc choice: "+pc;
+
+    resultsContainer.appendChild(playerH5);
+    resultsContainer.appendChild(pcH5);
+}
+
+function addOutcome(isWin){
+    outcome.classList.add('outcome');
+    outcome.textContent = isWin;
+    resultsContainer.appendChild(outcome);
+
+
+}
+
+function addResult(){
+    result.classList.add('result');
+    result.textContent = globalCounter;
+    resultsContainer.appendChild(result);
+
+    if(globalCounter == 5){
+        alert("")
+    }
+}
 
 
 
+//#endregion
 
+//#region Junk
 
 //remove this basically
 // function playGame(){
@@ -81,5 +143,8 @@ btnScissors.addEventListener('click',()=>{
 
 
 
+
+
+//#endregion
 
 
